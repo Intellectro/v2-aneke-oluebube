66a760e6f7187e894a56359be2403723d516e58f
intellectro <oluebubeaneke247@gmail.com>
2025-05-19 11:53:25 -0700

    feat: define initial routes for the application

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+    },
+    {
+        path: "/my/archive"
+    },
+    {
+        path: "/my/projects"
+    }
+];
